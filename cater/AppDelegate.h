a3b1a094e546ee3a96ed79bcca56e5d6eaf91156
jnc <jnc@jnctekiimac.(none)>
2013-05-30 16:29:19 +0800

    Initial Commit

---
+++ cater/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  cater
+//
+//  Created by jnc on 13-5-30.
+//  Copyright (c) 2013年 jnc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
